1114cef26fc5278dca5133d656551cfaf18e21c1
Arya Jagadditha <czajr13@gmail.com>
2026-01-19 00:08:31 +0700

    Fix: Tambahkan inline style untuk memastikan Google Sans font diterapkan

--- src/app/layout.js
+++ src/app/layout.js
@@ -103,7 +103,7 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Google+Sans:ital,opsz,wght@0,17..18,400..700;1,17..18,400..700&display=swap" rel="stylesheet" />
       </head>
-      <body className={`${inter.variable} font-sans`}>
+      <body className={`${inter.variable} font-sans`} style={{ fontFamily: "'Google Sans', sans-serif" }}>
         <Navbar />
         {children}
         <Footer />
